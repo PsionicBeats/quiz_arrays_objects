0f696e4320432606c37222602093a5917a1f3f76
Alex Aguinaldo <psionicbones@gmail.com>
2016-12-07 08:35:42 -1000

    update

---
+++ quiz_arrays_objects.js
@@ -0,0 +1,40 @@
+/*
+	BWAAAH
+	Alex Aguinaldo
+	Period 1
+	December 7, 2016
+	Arrays and Objects Quiz
+
+*/
+
+// ARRAYS Q-Q HALP SCREAMING. AAAAAAAA
+var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+function scream(level) {
+	if (level = true) {
+		console.log("Carry On My Wayward Son");
+	}else{
+		console.log("You shall not pass.")
+	}
+}
+
+level();
+
+// OBJECTS
+var batman = {
+	man : true,
+	utilityBelt: ["batarang", "grapplingHook", "sleepingGas", "smokePellet"],
+	vehicle: "batmobile",
+	sidekick: "robin",
+}
+
+function function_name(hero) {
+	if (hero == man) {
+		console.log("Robin! To the " + vehicle + "!" + "Bring your " + utilityBelt.2 + "!");
+	}else{
+		console.log("No, I'm the Bat");
+	}
+}
+
+hero(batman);
